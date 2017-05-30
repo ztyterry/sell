8b2f39b3634524e41321e5dcd76cace5e5b71dc5
ruowei <554006264@qq.com>
2017-05-30 12:03:59 +0800

    Creat object

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <div>this is goods</div>
+    </div>
+</template>
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
+<script type="text/ecmascript-6">
+    export default {};
+</script>
